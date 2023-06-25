3ef94212f39c03a4cb2f80df3abb3ad81ab663da
Amjad Ali <cowboyamjad@gmail.com>
2023-06-25 23:37:47 +0530

    Chnaged ui for desktop

--- src/Generate.js
+++ src/Generate.js
@@ -1,11 +1,12 @@
 import React,{useState,useEffect} from 'react'
 import Card from "./Card";
 import axios from "axios";
+import { useMediaQuery } from "react-responsive";
 
 
 const Generate = ({ text, setText, startDestination, endDestination, startDate, endDate }) => {
 
-
+    const isMobile = useMediaQuery({ maxWidth: 640 });
     const [img, setimg] = useState("")
     const formatDate = (dateString) => {
         const date = new Date(dateString);
@@ -73,12 +74,12 @@ const Generate = ({ text, setText, startDestination, endDestination, startDate, 
 
 
     return (<div className='bg-[#F3F4F6] '>
-                <div className='h-max w-full flex flex-col justify-center items-center '><img src={img} className='h-64 w-max ' alt={endDestination}/>
+        {isMobile?<><div className='h-max w-full flex flex-col justify-center items-center  '><img src={img} className='h-64 w-full sm:w-full sm:h-full  ' alt={endDestination}/>
         <div className='font-bold text-5xl capitalize w-full text-white  rounded-2xl px-4 py-2 -mt-32'>
         {startDestination} To {endDestination}
         </div>
-        </div>
-        <div className=' cardscontainer  h-full flex flex-col mx-4 '>
+        </div></>:<></>}
+        <div className=' cardscontainer  h-full flex flex-col mx-4 sm:mx-8 '>
         {text.map((day, index) => {
             return (<Card key={index} day={day} getImg={getImg}  />)
         })}
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Typewriter } from 'react-simple-typewriter'
+import { useMediaQuery } from "react-responsive";
 
 const Home = ({
     startDestination,
@@ -13,6 +14,7 @@ const Home = ({
     setEndDate,
 }) => {
 
+    const isMobile = useMediaQuery({ maxWidth: 640 });
     const captions = [
         "Plan Your Perfect Getaway with AI Assistance",
         "Effortless Trip Planning with Intelligent AI Tech",
@@ -23,8 +25,8 @@ const Home = ({
 
 
     return (
-        <div className="flex flex-col  items-center justify-center h-screen bg-mountain bg-cover font-main">
-            <div className="text-5xl font-bold text-white font-main h-64  w-full px-8 mb-16">
+        <div className="flex flex-col sm:flex-row  items-center justify-center h-screen bg-mountain bg-cover font-main ">
+            <div className="text-5xl sm:text-[5rem] font-bold text-white font-main h-64  w-full px-8 mb-16 sm:w-[40rem]  sm:justify-center sm:flex items-center">
             <Typewriter words={captions} loop={true} deleteSpeed={25} typeSpeed={50} delaySpeed={5000} />
             </div>
             <form className="flex flex-col space-y-4">
@@ -34,7 +36,7 @@ const Home = ({
                     placeholder="Start Destination"
                     value={startDestination}
                     onChange={(event) => setStartDestination(event.target.value)}
-                    className="px-4 py-2  rounded-2xl focus:outline-none inputs"
+                    className="px-4 py-2  rounded-2xl sm:rounded-[2rem] focus:outline-none inputs sm:w-[30rem] sm:h-[6rem] sm:text-2xl"
                 />
                 <input
                     type="text"
@@ -42,7 +44,7 @@ const Home = ({
                     placeholder="End Destination"
                     value={endDestination}
                     onChange={(event) => setEndDestination(event.target.value)}
-                    className="px-4 py-2  text-white rounded-2xl focus:outline-none inputs"
+                    className="px-4 py-2  rounded-2xl sm:rounded-[2rem] focus:outline-none inputs sm:w-[30rem] sm:h-[6rem] sm:text-2xl"
                 />
                 <input
                     type="date"
@@ -50,7 +52,7 @@ const Home = ({
                     placeholder="Start Date"
                     value={startDate}
                     onChange={(event) => setStartDate(event.target.value)}
-                    className="px-4 py-2  text-white rounded-2xl focus:outline-none inputs"
+                    className="px-4 py-2  rounded-2xl sm:rounded-[2rem] focus:outline-none inputs sm:w-[30rem] sm:h-[6rem] sm:text-2xl"
                 />
                 <input
                     type="date"
@@ -58,7 +60,7 @@ const Home = ({
                     placeholder="End Date"
                     value={endDate}
                     onChange={(event) => setEndDate(event.target.value)}
-                    className="px-4 py-2  text-white rounded-2xl focus:outline-none inputs"
+                    className="px-4 py-2  rounded-2xl sm:rounded-[2rem] focus:outline-none inputs sm:w-[30rem] sm:h-[6rem] sm:text-2xl"
                 />               
                     <Link to='/generate'>
                     <button
